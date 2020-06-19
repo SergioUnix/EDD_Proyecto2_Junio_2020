0e8b616ad73623665021e82e73fad2a2a3f9a7b8
SergioUnix <2416465290108@ingenieria.usac.edu.gt>
2020-06-19 17:29:52 -0600

    201020252 ___ SergioAriel__paquetes

--- src/llegarapidito/LlegaRapidito.java
+++ src/llegarapidito/LlegaRapidito.java
@@ -15,7 +15,49 @@ public class LlegaRapidito {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    //****************************************************************************************************    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
