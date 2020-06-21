37f8c872d24a635a02b8e1c5acbaae426516d69e
Alejandro <ale_lorenty@hotmail.com>
2020-06-20 23:07:20 -0600

    201800469
    estructura bloque

---
+++ src/estructuras/estructura_BloqueC.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+/**
+ *
+ * @author Alejandro Lorenty
+ */
+public class estructura_BloqueC {
+    
+}
